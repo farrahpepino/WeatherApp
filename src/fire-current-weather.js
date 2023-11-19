e6dbfd133d35547c59bc30005d46c57d575ef2d8
fpepino1 <115742428+fpepino1@users.noreply.github.com>
2023-11-19 00:25:51 -0500

    updated api on javascript and css

--- src/fire-current-weather.js
+++ src/fire-current-weather.js
@@ -1,3 +1,43 @@
+function getForecast(city) {
+  let apiKey = "010at9ca97fd6359640c0357507fb1co";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+  console.log(response.data);
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+         <div id="forecast-weather"><div>
+          <strong class="weather-forecast-day">${formatDay(day.time)}</strong>
+          <br />
+          <strong class="weather-forecast-temperature-max">${Math.round(
+            day.temperature.maximum
+          )}°C</strong>
+          <text class="weather-forecast-temperature-min">${Math.round(
+            day.temperature.minimum
+          )}°C</text>
+          </div>
+        </div>
+     `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast-weather");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return days[date.getDay()];
+}
+
 function refreshWeather(response) {
   let currentTemperatureElement = document.querySelector(
     "#current-temperature-value"
@@ -60,6 +100,8 @@ function refreshWeather(response) {
   currentDateElement.innerHTML = `${month} ${noDate} ${fullYear}`;
   let currentDayTimeElement = document.querySelector("#current-day-time");
   currentDayTimeElement.innerHTML = `${day} ${hour}:${minute}`;
+
+  getForecast(response.data.city);
 }
 
 function searchCity(city) {
--- src/fire-forecast.js
+++
@@ -1,4 +0,0 @@
-let apiKey = "010at9ca97fd6359640c0357507fb1co";
-let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={query}&key=${apiKey}&units=metric`;
-
-
